d110f5fd6bcc2398adaefe05727c5244d36c91d0
tony-owen.moubouyi-bandou <tony-owen.moubouyi-bandou@ens.uvsq.fr>
2025-06-13 21:07:12 +0200

    backend done

--- auth_api/serializers.py
+++ auth_api/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import authenticate
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import check_password
+
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     confirm_password = serializers.CharField(write_only=True)
@@ -29,3 +35,29 @@ class RegisterSerializer(serializers.ModelSerializer):
         validated_data.pop('confirm_password')
         user = CustomUser.objects.create_user(**validated_data)
         return user
+
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+        User = get_user_model()
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError(
+                {"non_field_errors": [_("Invalid email or password.")]}
+            )
+
+        if not check_password(password, user.password):
+            raise serializers.ValidationError(
+                {"non_field_errors": [_("Invalid email or password.")]}
+            )
+
+        attrs['user'] = user
+        return attrs
--- wellness_core/settings.py
+++ wellness_core/settings.py
@@ -37,13 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
+    'rest_framework_simplejwt',
     'auth_api',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,4 +129,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'auth_api.CustomUser'
-CORS_ALLOW_ALL_ORIGINS = True  # for development
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",  
+    "http://127.0.0.1:4200",
+
+]
